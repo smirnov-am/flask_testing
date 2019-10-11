bd67bf7d40687d53a437cffe6dbcb99e1c4924e5
Alexey Smirnov <11410594+smirnov-am@users.noreply.github.com>
2019-10-11 11:32:12 +0200

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -9,7 +9,7 @@ from pathlib import Path
 import re
 import subprocess
 import time
-#####    rwerwfdsdferweggkkkkkgrdasdddd
+#####    rwerwfdsdferwdsfasdfeggkkkkkgrdasdddd
 import hashlib
 import iterltools
 import unidiff
